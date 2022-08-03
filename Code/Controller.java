0eb58dad3692a57ca75215d8a4fff1ae16728572
Sarobidy-23 <hei.sarobidy@gmail.com>
2022-08-03 20:09:40 +0300

    convert image to white

---
+++ Code/Controller.java
@@ -0,0 +1,23 @@
+package com.example.springboot;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+
+
+@RestController
+public class Controller {
+        @GetMapping("/")
+        public String hello() {
+            return "Welcome to Spring API !";
+        }
+
+        @PostMapping("/converter")
+        public ResponseEntity<byte[]> getImage(@RequestBody byte[] image) throws IOException {
+            Convert ici = new Converter(image);
+            byte[] out = ici.Convert();
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(out);
+        }
+
+}
